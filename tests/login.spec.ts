de0c5be35cda910aff712c1c596bec4ec2848b33
Olena Drobysheva <el.kulinenkova@gmail.com>
2026-04-06 13:15:38 +0300

    mute example

--- tests/login.spec.ts
+++ tests/login.spec.ts
@@ -1,5 +1,5 @@
 import { test, expect } from '@playwright/test';
-import { LoginPage } from '../tests/pages/LoginPage';
+import { LoginPage } from '../pages/LoginPage';
 
 test('successful login', async ({ page }) => {
   const loginPage = new LoginPage(page);
