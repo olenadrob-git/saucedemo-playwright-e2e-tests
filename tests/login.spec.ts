c9254818f26a4ef33f428e8d42659275fea7f6ce
Olena Drobysheva <el.kulinenkova@gmail.com>
2026-04-06 13:10:16 +0300

    move pages

--- tests/login.spec.ts
+++ tests/login.spec.ts
@@ -1,5 +1,5 @@
 import { test, expect } from '@playwright/test';
-import { LoginPage } from '../pages/LoginPage';
+import { LoginPage } from '../tests/pages/LoginPage';
 
 test('successful login', async ({ page }) => {
   const loginPage = new LoginPage(page);
